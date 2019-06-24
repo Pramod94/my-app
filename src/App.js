f296b60611295d8034ebc434154bba0b1cd242b2
Pramod <pramodmithyantha@gmail.com>
2019-06-24 09:33:27 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>Hey It's Working !!</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
